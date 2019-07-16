3d1a96ebb01985c7ff3dcba181f50bbb1f121167
Neffats <steffan.mon@gmail.com>
2019-07-16 09:18:15 +0100

    Started on graphics handling.

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import chip8 "Github/Chip8/src"
 
+func main() {
+	g := chip8.Graphics{}
+
+	err := g.Init()
+	if err != nil {
+		panic(err)
+	}
+
+	err = g.Run()
+	if err != nil {
+		panic(err)
+	}
 }
--- src/graphics.go
+++ src/graphics.go
@@ -6,39 +6,45 @@ import (
 	"github.com/veandco/go-sdl2/sdl"
 )
 
+//Graphics handles the window for the Chip8.
 type Graphics struct {
 	m Memory
 
 	screen []int
 
-	window  sdl.Window
-	surface sdl.Surface
+	window  *sdl.Window
+	surface *sdl.Surface
 
 	w, h int
 }
 
+//Init initalises the sdl window.
 func (g *Graphics) Init() error {
 	if err := sdl.Init(sdl.INIT_EVERYTHING); err != nil {
 		return fmt.Errorf("could not initialise sdl: %v", err)
 	}
 
 	// TODO: Move this: defer sdl.Quit()
-
-	g.window, err := sdl.CreateWindow("test", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
+	// Stop "non-name on left side of :=" error
+	var err error
+	g.window, err = sdl.CreateWindow("test", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
 		800, 600, sdl.WINDOW_SHOWN)
 	if err != nil {
 		return fmt.Errorf("could not create sdl window: %v", err)
 	}
-	defer window.Destroy()
+	// TODO: Move this: defer window.Destroy()
 
-	g.surface, err := window.GetSurface()
+	g.surface, err = g.window.GetSurface()
 	if err != nil {
 		return fmt.Errorf("could not get sdl window surface: %v", err)
 	}
+	return nil
 }
 
+//Run starts the main loop for the graphics struct.
 func (g *Graphics) Run() error {
 	defer sdl.Quit()
+	defer g.window.Destroy()
 
 	g.surface.FillRect(nil, 0)
 
@@ -57,4 +63,6 @@ func (g *Graphics) Run() error {
 			}
 		}
 	}
+
+	return nil
 }
